33bbb8a2e911adc2fb2e7bba305130ea918d925c
Ashok Akula <ashok.akula@tammina.in>
2025-10-30 14:46:00 +0530

    changeA

--- server/HRMS_Server/src/services/leave.services.ts
+++ server/HRMS_Server/src/services/leave.services.ts
@@ -3,7 +3,7 @@ import { LeaveBalance, LeaveRequest } from '../interface/leave-interface';
 export default class LeaveService {
   public static async createLeaveBalance(data: LeaveBalance) {
     const [rows] = await pool.query(
-      `INSERT INTO leave_balance (employee_id, leave_year_start, leave_year_end, casual_leave_allocated, marriage_leave_allocated, comp_offs_allocated, medical_leave_allocated, paid_leave_allocated) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+      `INSERT INTO leave_balance (employee_id, leave_year_start, leave_year_end, casual_leave_allocated, marriage_leave_allocated, comp_offs_allocated, sick_leave_allocated, paid_leave_allocated) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
       [
         data.employee_id,
         data.leave_year_start,
@@ -11,7 +11,7 @@ export default class LeaveService {
         data.casual_leave_allocated || 0,
         data.marriage_leave_allocated || 0,
         data.comp_offs_allocated || 0,
-        data.medical_leave_allocated || 0,
+        data.sick_leave_allocated || 0,
         data.paid_leave_allocated || 0,
       ]
     );
@@ -40,4 +40,39 @@ export default class LeaveService {
     const [rows] = await pool.query('SELECT * FROM leave_requests');
     return rows;
   }
+
+  public static async addLeaves(data: LeaveBalance) {
+
+  console.log('Received leave data:', data);
+
+  const [rows] = await pool.query(
+    `INSERT IGNORE INTO leaves (
+      employee_id, employee_number, full_name, leave_year_start, leave_year_end,
+      casual_leave_allocated, marriage_leave_allocated,
+      comp_offs_allocated, sick_leave_allocated,
+      paid_leave_allocated
+    ) 
+    SELECT e.employee_id, e.employee_number, e.full_name, ?, ?, ?, ?, ?, ?, ?
+    FROM employees e`,
+    [
+      data.leave_year_start,
+      data.leave_year_end,
+      data.casual_leave_allocated ,
+      data.marriage_leave_allocated ,
+      data.comp_offs_allocated ,
+      data.sick_leave_allocated ,
+      data.paid_leave_allocated 
+    ]
+  );
+  return rows;
+}
+
+  public static async getLeavesByEmployeeId(employeeId: number) {
+    const [rows] = await pool.query(
+      'SELECT * FROM leaves WHERE employee_id = ?',
+      [employeeId]
+    );
+    return rows;
+  }
+
 }
---
+++ src/app/services/leave.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: "root", 
+})
+
+export class LeaveService {
+    private apiUrl = "http://localhost:3562/api"; 
+
+    constructor(private http: HttpClient) {}
+
+    getLeaves(employeeId: number): Observable<any> {
+        return this.http.post<any>(`${this.apiUrl}/v1/get-leaves`, { employee_id: employeeId });
+    }
+}
