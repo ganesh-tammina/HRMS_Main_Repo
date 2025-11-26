9be5b74a5ee2be700248fb4f81ebe6e9b56f4191
Ashok <114016984+AkulaAshok@users.noreply.github.com>
2025-11-26 15:45:11 +0530

    updated query to insert work-report

--- server/HRMS_Server/src/work-track/services/work-track.service.ts
+++ server/HRMS_Server/src/work-track/services/work-track.service.ts
@@ -13,13 +13,14 @@ export class WorkTrackService {
       end_time: h.end_time,
       task: h.task,
       project: h.project,
-      type: h.type
+      type: h.type,
+      technology: JSON.stringify(data.technologies)
     }));
       
     for (const hour of result) {
       await pool.query(
-        `INSERT INTO work_report (employee_id, date, task_start_time, task_end_time, task, project, type) VALUES (?, ?, ?, ?, ?, ?, ?)`,
-        [hour.employee_id, hour.date, hour.start_time, hour.end_time, hour.task, hour.project, hour.type]
+        `INSERT INTO work_report (employee_id, date, task_start_time, task_end_time, task, project, type, technology) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+        [hour.employee_id, hour.date, hour.start_time, hour.end_time, hour.task, hour.project, hour.type, hour.technology]
       );
     }
     return result;
