88dad3b969488c1473bc7a9aed85dbb3929feae9
Ashok <114016984+AkulaAshok@users.noreply.github.com>
2025-11-28 18:37:12 +0530

    added validations to remote-clock-in

--- server/HRMS_Server/src/routes/remote-clockin-route.ts
+++ server/HRMS_Server/src/routes/remote-clockin-route.ts
@@ -4,14 +4,16 @@ import { RemoteClockInController } from '../controller/remote-clock-in-controlle
 export const remoteClockInRouter = Router();
 
 remoteClockInRouter.post('/create', RemoteClockInController.createRemoteClockIn);
-remoteClockInRouter.post('/get-by-id', RemoteClockInController.getRemoteClockInById);
+
+
+/* remoteClockInRouter.post('/get-by-id', RemoteClockInController.getRemoteClockInById);*/
 remoteClockInRouter.get('/get-all', RemoteClockInController.getAllRemoteClockIns);
-remoteClockInRouter.post('/update', RemoteClockInController.updateRemoteClockIn);
+
+/* remoteClockInRouter.post('/update', RemoteClockInController.updateRemoteClockIn);
 remoteClockInRouter.post('/get-by-date', RemoteClockInController.getRemoteClockInsByDate);
 remoteClockInRouter.post('/get-requests', RemoteClockInController.getRemoteClockInRequests);
 remoteClockInRouter.post('/get-by-status', RemoteClockInController.getRemoteClockInsByStatus);
 remoteClockInRouter.post('/get-by-manager', RemoteClockInController.getRemoteClockInsByManager);
 remoteClockInRouter.post('/update-status', RemoteClockInController.updateRemoteClockInStatus);
 remoteClockInRouter.post('/update-clock-out', RemoteClockInController.updateRemoteClockInClockOut);
-remoteClockInRouter.post('/delete', RemoteClockInController.deleteRemoteClockIn);
-
+*/
