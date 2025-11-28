fab76464bbda077de8bba48eeb8e0a8589848ae7
bipulsardar421 <bipulsardar421@gmail.com>
2025-11-28 18:55:32 +0530

    656

--- server/HRMS_Server/src/services/work-from-home-service.ts
+++ server/HRMS_Server/src/services/work-from-home-service.ts
@@ -2,6 +2,13 @@ import { pool } from '../config/database';
 import { WorkFromHome } from '../interface/work-from-home.interface';
 
 export class WorkFromHomeService {
+  static async getMyNotified(myId: string) {
+    const [adfasd]: any = await pool.query(
+      'select w.* from work_from_home w join notified_user n ON w.notify_id = n.notify_id where n.employee_id = ?',
+      [myId]
+    )
+    return adfasd;
+  }
   public static async applyWorkFromHome(data: WorkFromHome) {
     const [notifyResult]: any = await pool.query(
       `INSERT INTO notified_user (employee_id) VALUES (?)`,
@@ -32,30 +39,78 @@ export class WorkFromHomeService {
 
   public static async getWFHRequestsByEmployeeId(employee_id: number) {
     const [rows]: any = await pool.query(
-      `SELECT * FROM work_from_home WHERE employee_id = ?`,
+      `SELECT 
+    w.id,
+    w.employee_id AS employee_id_applied,
+    k.full_name AS applied_user_name,
+    n.employee_id AS employeee_id_notified,
+    e.full_name AS notified_user_name,
+    w.reason,
+    w.from_date,
+    w.to_date,
+    w.total_days,
+    w.type,
+    n.action_taken
+    FROM
+    work_from_home w
+        LEFT JOIN
+    notified_user n ON w.notify_id = n.notify_id
+        LEFT JOIN
+    employees e ON n.employee_id = e.employee_id
+        LEFT JOIN
+    employees k ON k.employee_id = w.employee_id where w.employee_id = ? and n.action_taken = 'pending'`,
       [employee_id]
     );
-    return rows;
+    return rows.length > 0 ? rows : 'No Request Found';
   }
 
   public static async getAllWFHRequests() {
     const [rows]: any = await pool.query(
-      `SELECT 
-          w.*, n.action_taken 
-       FROM work_from_home w
-       LEFT JOIN notified_user n 
-            ON w.notify_id = n.notify_id`
+      `SELECT  
+    w.id,
+    w.employee_id AS employee_id_applied,
+    k.full_name AS applied_user_name,
+    n.employee_id AS employeee_id_notified,
+    e.full_name AS notified_user_name,
+    w.reason,
+    w.from_date,
+    w.to_date,
+    w.total_days,
+    w.type,
+    n.action_taken
+    FROM
+    work_from_home w
+        LEFT JOIN
+    notified_user n ON w.notify_id = n.notify_id
+        LEFT JOIN
+    employees e ON n.employee_id = e.employee_id
+        LEFT JOIN
+    employees k ON k.employee_id = w.employee_id`
     );
     return rows;
   }
 
   public static async updateWFHRequestStatus(
+    notifier_id: number,
     request_id: number,
     status: string
   ) {
+    if (status === 'approved' || status === 'cancelled') {
+      return 'Invalid status type.';
+    }
+    const [wfh]: any = await pool.query(
+      'select w.notify_id, n.employee_id as notified_user from work_from_home w join notified_user n on w.notify_id = n.notify_id where w.id = ?',
+      [request_id]
+    );
+    if (wfh.length === 0) {
+      return 'No Data Found. Invalid request';
+    }
+    if (wfh[0].notified_user != notifier_id) {
+      return 'You are not authorized to perform this action.';
+    }
     const [result]: any = await pool.query(
-      `UPDATE work_from_home SET status = ? WHERE id = ?`,
-      [status, request_id]
+      `UPDATE notified_user SET status = ? WHERE id = ?`,
+      [status, wfh[0].notify_id]
     );
     return result;
   }
