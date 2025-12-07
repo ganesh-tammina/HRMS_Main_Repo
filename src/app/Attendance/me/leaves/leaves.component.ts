1e11d9211bdb60e048b0d4c43a8b8ca8a301c0a2
ramya.v@tammina.com <ramya.v@tammina.com>
2025-12-07 17:48:14 +0530

    Bug 50 fixed

--- src/app/Attendance/me/leaves/leaves.component.ts
+++ src/app/Attendance/me/leaves/leaves.component.ts
@@ -48,6 +48,14 @@ export class LeavesComponent implements OnInit {
   total_days: number = 0;
   description = '';
 wordsCount = 0;
+minDate: string = new Date().toISOString().split('T')[0];
+
+isWeekday = (dateIsoString: string) => {
+  const date = new Date(dateIsoString);
+  const day = date.getDay();
+  // 0 = Sunday, 6 = Saturday → disable these
+  return day !== 0 && day !== 6;
+};
 
   constructor(
     private candidateService: CandidateService,
@@ -140,6 +148,14 @@ wordsCount = 0;
   }
 
   submitRequest() {
+      const start = new Date(this.leaveForm.value.start_date);
+  const end = new Date(this.leaveForm.value.end_date);
+
+  if (start.getDay() === 0 || start.getDay() === 6 ||
+      end.getDay() === 0 || end.getDay() === 6) {
+    alert("Cannot apply leave on weekends!");
+    return;
+  }
     if (this.leaveForm.invalid || this.total_days <= 0) {
       this.leaveForm.markAllAsTouched();
       this.presentToast('Please fill all required fields and ensure dates are valid.', 'warning');
