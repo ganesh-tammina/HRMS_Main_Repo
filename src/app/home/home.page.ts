6ac705ea030776b38469175df8bd1726e45ce441
praveena-tammina <50910978+praveena-tammina@users.noreply.github.com>
2025-12-10 11:28:34 +0530

    New home page UI changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,7 @@ import { IonicModule, AlertController } from '@ionic/angular';
 import { CandidateService } from '../services/pre-onboarding.service';
 import { ClockButtonComponent } from '../services/clock-button/clock-button.component';
 import { RouteGuardService } from '../services/route-guard/route-service/route-guard.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   standalone: true,
@@ -29,7 +30,7 @@ export class HomePage implements OnInit {
   employee_id: any;
   uploadedImageUrl: string | null = null;
   imageUrls: any;
-
+  profileimg: string = environment.apiURL;
   backgroundImageUrl: string = '../../assets/holidays-pics/holidays-img.svg';
 
   constructor(
@@ -63,7 +64,11 @@ export class HomePage implements OnInit {
             this.one = this.allEmployees[0];
             this.fullName = this.one[0].reporting_to;
             this.employee_id = this.one[0].employee_id;
-
+            if (this.one[0].image) {
+              this.imageUrls = `https://${this.profileimg}${this.one[0].image}`;
+            } else {
+              this.imageUrls = '../../../assets/user.svg';
+            }
             console.log('profile', this.imageUrls);
             localStorage.setItem('employee_id', this.employee_id);
             this.candidateService.setLoggedEmployeeId(this.employee_id);
